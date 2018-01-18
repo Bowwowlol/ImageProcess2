c7ada9ca59094fb82889a0c0578484daa7a702bf
bbbennn1123 <bbbennn1123@hotmail.com>
2018-01-18 16:19:19 +0800

    change 20180118.

--- MFCApplication3/CVisualTracker.h
+++ MFCApplication3/CVisualTracker.h
@@ -34,12 +34,24 @@ typedef struct
 
 } VT_Params;
 
+//typedef enum {TM_SQDIFF = CV_TM_SQDIFF, TM_SQDIFF_NORMED, TM_CCORR, TM_CCORR_NORMED,TM_CCOEFF, TM_CCOEFF_NORMED} TempMatchParam;
+
+typedef enum  {TempMatch = 1, MeanShift, CAMShift} VT_MethodType;
+
 
 class CVisualTracker {
 public:
 	CVisualTracker();
 	~CVisualTracker();
 	void SetMethodType(VT_MethodType Type);
+	VT_MethodType GetMethodType() const;
+	void SetVT_Params(VT_MethodType Type, VT_Params Param);
+	VT_Params GetVT_Params() const;
+	void SetROI(cv::Mat* pROI);
+	cv::Mat* GetROI() const;
+	BOOL Tracking(cv::Mat &Frame, cv::Rect &TrackRect);
+	void ShowResult(cv::Mat &Frame, cv::Rect &TrackRect);
+
 
 
 private:
